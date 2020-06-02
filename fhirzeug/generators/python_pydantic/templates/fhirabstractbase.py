8a492f408b77ef98eb0042740d8aeef20617eed1
Julian David Rath <julian@skalarsystems.com>
2020-06-02 14:05:36 +0200

    removed _without_empty_items, so we can put that in a seperate pr

--- fhirzeug/generators/python_pydantic/templates/fhirabstractbase.py
+++ fhirzeug/generators/python_pydantic/templates/fhirabstractbase.py
@@ -18,34 +18,6 @@ def camelcase_alias_generator(name: str) -> str:
     return components[0] + "".join(word.capitalize() for word in components[1:])
 
 
-def _without_empty_items(obj: typing.Any):
-    """
-    Clean empty items: https://www.hl7.org/fhir/datatypes.html#representations
-    TODO: add support for extensions: https://www.hl7.org/fhir/json.html#null
-    """
-    if isinstance(obj, Mapping):
-        cleaned = {}
-        for key, value in obj.items():
-            cleaned_value = _without_empty_items(value)
-            if cleaned_value is not None:
-                cleaned[key] = cleaned_value
-        if cleaned:
-            return cleaned
-        return None
-    if isinstance(obj, str):
-        if not obj:
-            return None
-        return obj
-
-    if isinstance(obj, (list, tuple)):
-        cleaned = [_without_empty_items(item) for item in obj]
-        if any((item is not None for item in cleaned)):
-            return cleaned
-        return None
-
-    return obj
-
-
 class FHIRAbstractBase(pydantic.BaseModel):
     """Abstract base class for all FHIR elements.
     """
@@ -53,7 +25,3 @@ class FHIRAbstractBase(pydantic.BaseModel):
     class Config:
         alias_generator = camelcase_alias_generator
         allow_population_by_field_name = True
-
-    # def dict(self, *args, **kwargs):
-    #     serialized = super().dict(*args, **kwargs)
-    #     return _without_empty_items(serialized)
