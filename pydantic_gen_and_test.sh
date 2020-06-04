3133cf0a373cbf02d5a68e970ae3f6705ad67b6d
Julian David Rath <julian@skalarsystems.com>
2020-06-04 08:16:11 +0200

    Resource Members do work now (#14)
    * all tests except 5 are passing
    * All Resource members do have a factory now

--- pydantic_gen_and_test.sh
+++ pydantic_gen_and_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf ../pydantic-fhir
 poetry run fhirzeug  --output-directory ../pydantic-fhir --generator python_pydantic
 cd ../pydantic-fhir
-poetry run pytest tests -v
+poetry run pytest tests -vv
