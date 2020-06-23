f7617671c8c226a18480d7e924ba0d37b334ce68
Wauplin <lucainp@gmail.com>
2020-06-23 11:35:03 +0200

    FIX data validation + add unit tests

--- fhirzeug/generators/python_pydantic/templates/fhir_basic_types.py
+++ fhirzeug/generators/python_pydantic/templates/fhir_basic_types.py
@@ -1,21 +1,28 @@
 import pydantic
 
-FHIRDateTime = pydantic.constr(
-    regex="([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?"
-)
-FHIRDate = pydantic.constr(
-    regex="([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1]))?)?"
-)
-FHIRInstant = pydantic.constr(
-    regex="([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00))"
-)
-FHIRTime = pydantic.constr(
-    regex="([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?"
-)
-FHIRCode = pydantic.constr(regex="[^\s]+(\s[^\s]+)*")
 
-FHIROid = pydantic.constr(regex="urn:oid:[0-2](\.(0|[1-9][0-9]*))+")
+def exact_regex_constr(**kwargs):
+    if kwargs.get("regex") is not None:
+        kwargs["regex"] = r"\A" + kwargs["regex"].lstrip(r"\A").rstrip(r"\Z") + r"\Z"
+    return pydantic.constr(**kwargs)
 
-FHIRId = pydantic.constr(regex="[A-Za-z0-9\-\.]{1,64}")
 
-FHIRBase64Binary = pydantic.constr(regex="(\s*([0-9a-zA-Z\+\=]){4}\s*)+")
+FHIRDateTime = exact_regex_constr(
+    regex=r"([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?"
+)
+FHIRDate = exact_regex_constr(
+    regex=r"([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1]))?)?"
+)
+FHIRInstant = exact_regex_constr(
+    regex=r"([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00))"
+)
+FHIRTime = exact_regex_constr(
+    regex=r"([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?"
+)
+FHIRCode = exact_regex_constr(regex=r"[^\s]+(\s[^\s]+)*")
+
+FHIROid = exact_regex_constr(regex=r"urn:oid:[0-2](\.(0|[1-9][0-9]*))+")
+
+FHIRId = exact_regex_constr(regex=r"[A-Za-z0-9\-\.]{1,64}")
+
+FHIRBase64Binary = exact_regex_constr(regex=r"(\s*([0-9a-zA-Z\+\=]){4}\s*)+")
--- tests/pydantic/test_basic_types_pyd.py
+++ tests/pydantic/test_basic_types_pyd.py
@@ -14,7 +14,7 @@ from fhirzeug.generators.python_pydantic.templates.fhir_basic_types import (
 )
 
 
-class TestModel(pydantic.BaseModel):
+class ExampleModel(pydantic.BaseModel):
     """A Test class to check all basic datatypes."""
 
     date: typing.Optional[FHIRDate]
@@ -30,71 +30,83 @@ def test_fhirdate():
     """Test FHIRDate
     https://www.hl7.org/fhir/datatypes.html#date"""
     with pytest.raises(pydantic.ValidationError):
-        model = TestModel(date="foo")
+        model = ExampleModel(date="foo")
     with pytest.raises(pydantic.ValidationError):
-        model = TestModel(date="24:00")
-    model = TestModel(date="1905-08-23")
+        model = ExampleModel(date="24:00")
+    with pytest.raises(pydantic.ValidationError):
+        model = ExampleModel(date="1973-06-33")
+    model = ExampleModel(date="2018")
+    model = ExampleModel(date="1973-06")
+    model = ExampleModel(date="1905-08-23")
 
 
 def test_fhirdatetime():
     """Test FHIRDateTime
     https://www.hl7.org/fhir/datatypes.html#dateTime"""
     with pytest.raises(pydantic.ValidationError):
-        model = TestModel(datetime="foo")
+        model = ExampleModel(datetime="foo")
     with pytest.raises(pydantic.ValidationError):
-        model = TestModel(datetime="24:00")
-    model = TestModel(datetime="1905-08-23")
+        model = ExampleModel(datetime="24:00")
+    with pytest.raises(pydantic.ValidationError):
+        model = ExampleModel(datetime="2015-02-07T13:28:17")
+    with pytest.raises(pydantic.ValidationError):
+        model = ExampleModel(datetime="1905-08-33")
+    model = ExampleModel(datetime="2018")
+    model = ExampleModel(datetime="1973-06")
+    model = ExampleModel(datetime="1905-08-23")
+    model = ExampleModel(datetime="2015-02-07T13:28:17-05:00")
+    model = ExampleModel(datetime="2017-01-01T00:00:00.000Z")
 
 
 def test_fhirtime():
     """Test FHIRTime
     https://www.hl7.org/fhir/datatypes.html#time"""
     with pytest.raises(pydantic.ValidationError):
-        model = TestModel(time="foo")
+        model = ExampleModel(time="foo")
     with pytest.raises(pydantic.ValidationError):
-        model = TestModel(time="24:00")
-    model = TestModel(time="17:00:00")
+        model = ExampleModel(time="24:00")
+    with pytest.raises(pydantic.ValidationError):
+        model = ExampleModel(time="13:28:17-05:00")
+    model = ExampleModel(time="17:00:00")
 
 
 def test_fhirinstant():
     """Test FHIRInstant
     https://www.hl7.org/fhir/datatypes.html#instant"""
     with pytest.raises(pydantic.ValidationError):
-        model = TestModel(instant="foo")
+        model = ExampleModel(instant="foo")
     with pytest.raises(pydantic.ValidationError):
-        model = TestModel(instant="24:00")
-    model = TestModel(instant="2017-01-01T00:00:00Z")
-    model = TestModel(instant="2015-02-07T13:28:17.239+02:00")
+        model = ExampleModel(instant="24:00")
+    model = ExampleModel(instant="2017-01-01T00:00:00Z")
+    model = ExampleModel(instant="2015-02-07T13:28:17.239+02:00")
 
 
 def test_fhirbase64binary():
-
     """Test FHIRBase64Binary
     https://www.hl7.org/fhir/datatypes.html#base64binary"""
     message = "Foo Bar"
     message_bytes = message.encode("ascii")
     base64_bytes = base64.b64encode(message_bytes)
     base64_message = base64_bytes.decode("ascii")
-    model = TestModel(base64=base64_message)
+    model = ExampleModel(base64=base64_message)
 
     with pytest.raises(pydantic.ValidationError):
-        model = TestModel(base64=message)
+        model = ExampleModel(base64=message)
 
 
 def test_fhiroid():
-
     """Test FHIROid
     https://www.hl7.org/fhir/datatypes.html#oid"""
 
-    model = TestModel(oid="urn:oid:1.2.3.4.5")
+    model = ExampleModel(oid="urn:oid:1.2.3.4.5")
     with pytest.raises(pydantic.ValidationError):
-        model = TestModel(oid="foo")
+        model = ExampleModel(oid="foo")
 
 
 def test_fhirid():
     """Test FHIRId
     https://www.hl7.org/fhir/datatypes.html#id"""
 
-    model = TestModel(fhir_id="foo.bar")
+    model = ExampleModel(fhir_id="foo.bar")
     with pytest.raises(pydantic.ValidationError):
-        model = TestModel(fhir_id="?")
+        model = ExampleModel(fhir_id="?")
