516a0ec27dcca89490c9f7d9f6b598b8f3679503
Wauplin <lucainp@gmail.com>
2020-07-14 17:00:42 +0200

    forgot to commit the test

---
+++ fhirzeug/generators/python_pydantic/static_files/tests/test_examples/test_basic.py
@@ -0,0 +1,12 @@
+import typing
+
+
+from pydantic_fhir import r4
+
+
+def test_bundle_entry_instanciation() -> None:
+    """Test that BundleEntry can be created with an existing Resource."""
+    issue = r4.OperationOutcomeIssue(code="not-found", severity="warning")
+    outcome = r4.OperationOutcome(issue=[issue])
+    entry = r4.BundleEntry(resource=outcome)
+    assert entry.resource.issue[0].code == "not-found"
