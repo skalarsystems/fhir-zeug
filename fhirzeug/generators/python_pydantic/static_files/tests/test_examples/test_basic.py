d03d5f771f4105a767c8db17ee2e1e870b9b2f67
Wauplin <lucain@skalarsystems.com>
2020-08-04 15:06:14 +0200

    FIX test

--- fhirzeug/generators/python_pydantic/static_files/tests/test_examples/test_basic.py
+++ fhirzeug/generators/python_pydantic/static_files/tests/test_examples/test_basic.py
@@ -46,7 +46,7 @@ def test_generated_enums() -> None:
 def test_empty_list_serialization() -> None:
     """An empty coding must be ignored during serialization.
 
-    - Expected behavior : "tag" = []
-    - Previous behavior : "tag" = [None]
+    - Expected behavior : {}
+    - Previous behavior : {"tag" = [None]}
     """
-    assert r4.Meta(tag=[r4.Coding()]) == {"tag": []}
+    assert r4.Meta(tag=[r4.Coding()]).dict() == {}
