53f453da20401e18aedd6d4358c91f91b8de9646
Wauplin <lucain@skalarsystems.com>
2020-10-19 10:01:41 +0200

    refacot tests

--- fhirzeug/generators/python_pydantic/static_files/tests/test_extension.py
+++ fhirzeug/generators/python_pydantic/static_files/tests/test_extension.py
@@ -5,32 +5,55 @@ import pydantic
 from pydantic_fhir import r4
 
 
-def test_extension_validation() -> None:
-    """Test Extension element validation."""
-    url = "test/example"
-    value_integer = 45
-    value_human_name = r4.HumanName(given=["Queen Elisabeth"])
+URL = "test/example"
+VALUE_INTEGER = 45
+VALUE_HUMAN_NAME = r4.HumanName(given=["Queen Elisabeth"])
 
-    integer_extension = r4.Extension(url=url, value_integer=value_integer)
-    human_name_extension = r4.Extension(url=url, value_human_name=value_human_name)
 
-    r4.Extension(url=url, extension=[integer_extension])
-    r4.Extension(url=url, extension=[human_name_extension])
-    r4.Extension(url=url, extension=[integer_extension, human_name_extension])
+def test_single_value_extension() -> None:
+    """Test an extension with a value is valid."""
+    r4.Extension(url=URL, value_integer=VALUE_INTEGER)
+    r4.Extension(url=URL, value_human_name=VALUE_HUMAN_NAME)
 
+
+def test_extension_with_subextensions() -> None:
+    """Test an extension with one or several subextensions but no values is valid."""
+    r4.Extension(
+        url=URL, extension=[r4.Extension(url=URL, value_integer=VALUE_INTEGER)],
+    )
+    r4.Extension(
+        url=URL, extension=[r4.Extension(url=URL, value_human_name=VALUE_HUMAN_NAME)],
+    )
+    r4.Extension(
+        url=URL,
+        extension=[
+            r4.Extension(url=URL, value_integer=VALUE_INTEGER),
+            r4.Extension(url=URL, value_human_name=VALUE_HUMAN_NAME),
+        ],
+    )
+
+
+def test_multiple_values_extension_forbidden() -> None:
+    """Test an extension with multiple values is forbidden."""
     with pytest.raises(pydantic.ValidationError):
         r4.Extension(
-            url=url, value_integer=value_integer, value_human_name=value_human_name
+            url=URL, value_integer=VALUE_INTEGER, value_human_name=VALUE_HUMAN_NAME
         )
 
+
+def test_value_and_subextension_forbidden() -> None:
+    """Test an extension with a value and a subextension is forbidden."""
     with pytest.raises(pydantic.ValidationError):
         r4.Extension(
-            url=url, value_integer=value_integer, extension=[human_name_extension]
+            url=URL,
+            value_integer=VALUE_INTEGER,
+            extension=[r4.Extension(url=URL, value_human_name=VALUE_HUMAN_NAME)],
         )
 
 
 def test_extension_in_a_domain_resource() -> None:
-    """Test Extension element validation, within a DomainResource."""
+    """Test Extension validation within a DomainResource at multiple levels."""
+    # Data is a valid definition of an extended domain resource
     data = {
         "resourceType": "DomainResource",
         "extension": [
@@ -48,9 +71,13 @@ def test_extension_in_a_domain_resource() -> None:
     }
     r4.from_dict(data)
 
+    # An empty list of extension is added to an extension containing a value.
+    # Extension is still valid since empty list is ignored.
     data["extension"][0]["extension"][1]["extension"] = []  # type: ignore
     r4.from_dict(data)
 
+    # An extension is added to a subextension that already contains a value.
+    # Extension is therefore forbidden.
     data["extension"][0]["extension"][1]["extension"] = [{"url": "test/example", "valueInteger": 45}]  # type: ignore
     with pytest.raises(pydantic.ValidationError):
         r4.from_dict(data)
