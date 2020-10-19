0ea999cf36baa11757c8ebb13bc5fa1c8e019d18
Wauplin <lucainp@gmail.com>
2020-10-19 12:06:39 +0200

    59 fix when list instead of dict (#65)
    * Add test that should fail
    
    * FIX cannot pass list instead of dict
    
    * Add test that should fail
    
    * FIX cannot pass list instead of dict
    
    * Add test that should fail
    
    * FIX cannot pass list instead of dict
    
    * add comment

--- tests/pydantic/test_fhir_base.py
+++ tests/pydantic/test_fhir_base.py
@@ -1,5 +1,6 @@
-"""Test FHIRAbstractBase object."""
-import pydantic
+"""Test FHIRAbstractBase Model."""
+import typing
+
 import pytest
 from pydantic import ValidationError
 
@@ -8,81 +9,25 @@ from fhirzeug.generators.python_pydantic.templates.resource_header import (
 )
 
 
-class ExampleModel(FHIRAbstractBase):
-    """Simple resource with a boolean field."""
-
-    field_a: bool
-
-
-class ChildModel(ExampleModel):
-    """Child model."""
-
-    pass
+class ItemModel(FHIRAbstractBase):
+    field_a: typing.Optional[str]
+    field_b: typing.Optional[str]
 
 
-class AnotherExampleModel(FHIRAbstractBase):
-    """Another resource that has a field of type ChildModel."""
-
-    field_example: ChildModel
+class ContainerModel(FHIRAbstractBase):
+    field_c: ItemModel
 
 
 def test_unknown_fields_not_allowed():
+    """Test unknown fields are not allowed in FHIRAbstractBase models."""
+    ItemModel()
     with pytest.raises(ValidationError):
-        ExampleModel(unknown_field=True)
+        ItemModel(unknown_field=True)
 
 
-def test_dynamic_validator():
-    """Test dynamic validators with different inheritance use cases."""
-    # 1. First case : field_a can take any boolean values in any classes.
-    ExampleModel(field_a=True)
-    ExampleModel(field_a=False)
-    ChildModel(field_a=True)
-    ChildModel(field_a=False)
-    AnotherExampleModel(field_example={"field_a": True})
-    AnotherExampleModel(field_example={"field_a": False})
+def test_list_instead_of_dict_must_fail():
+    """Test a list must not be validated as a dict in FHIRAbstractBase models."""
+    ContainerModel(field_c={"field_a": "123", "field_b": "456"})
 
-    # 2. Second case : field_a can only take True values in all classes.
-    # Starting from now, field_a value must be True
-    ExampleModel._add_post_root_validator(_must_be_true)
-    ExampleModel(field_a=True)
-    with pytest.raises(pydantic.ValidationError):
-        ExampleModel(field_a=False)
-
-    ChildModel(field_a=True)
-    with pytest.raises(pydantic.ValidationError):
-        ChildModel(field_a=False)
-
-    AnotherExampleModel(field_example={"field_a": True})
-    with pytest.raises(pydantic.ValidationError):
-        AnotherExampleModel(field_example={"field_a": False})
-
-    # 3. Third use case : field_a has no valid values for class ChildExample
-    #    Class AnotherExampleModel is also concerned but not ExampleModel.
-    #    Previous rule still apply on all classes.
-    ChildModel._add_post_root_validator(_must_be_false)
-
-    ExampleModel(field_a=True)
-    with pytest.raises(pydantic.ValidationError):
-        ExampleModel(field_a=False)
-
-    with pytest.raises(pydantic.ValidationError):
-        ChildModel(field_a=True)
-    with pytest.raises(pydantic.ValidationError):
-        ChildModel(field_a=False)
-
-    with pytest.raises(pydantic.ValidationError):
-        AnotherExampleModel(field_example={"field_a": True})
-    with pytest.raises(pydantic.ValidationError):
-        AnotherExampleModel(field_example={"field_a": False})
-
-
-def _must_be_true(values):
-    """Root validator to be added to ExampleModel."""
-    assert values.get("field_a")
-    return values
-
-
-def _must_be_false(values):
-    """Root validator to be added to ChildModel."""
-    assert not values.get("field_a")
-    return values
+    with pytest.raises(ValidationError):
+        ContainerModel(field_c=[{"field_a": "123", "field_b": "456"}])
